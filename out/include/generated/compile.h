4fc1eae7f3d6cc1e17b4e9b20c3ffd1492c01d3b
mikhailsimon <mikhailsimon0705@gmail.com>
2023-02-06 14:17:44 +0800

    Nethunter r5x: v1.1

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#2 SMP PREEMPT Sun Feb 5 20:23:53 +08 2023"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Feb 6 13:41:04 +08 2023"
 #define LINUX_COMPILE_BY "mikhailsimon"
-#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_HOST "kali"
 #define LINUX_COMPILER "xRageTC clang version 15.0.0 (https://github.com/llvm/llvm-project 4f97aa7e1d70f1b259e5fddd85de05235b01b192)"
 #define LINUX_COMPILER "xRageTC clang version 15.0.0, LLD 15.0.0"
