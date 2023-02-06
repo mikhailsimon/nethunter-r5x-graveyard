68065385fa131e980d191eb6ad1d4c75224187e0
mikhailsimon <mikhailsimon0705@gmail.com>
2023-02-06 12:44:26 +0800

    new menuconfig.sh script

---
+++ menuconfig.sh
@@ -0,0 +1,3 @@
+make O=out ARCH=arm64 vendor/RMX1911_nethunter_defconfig && make O=out ARCH=arm64 menuconfig
+cp out/.config arch/arm64/configs/vendor/RMX1911_nethunter_defconfig
+echo "New RMX1911_nethunter_defconfig saved!"
