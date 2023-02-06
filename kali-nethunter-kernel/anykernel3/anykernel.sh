d6e3ccac35033a91a97c65261289443b6388ee03
mikhailsimon <mikhailsimon0705@gmail.com>
2023-02-06 15:28:51 +0800

    removed unneccessary files

--- kali-nethunter-kernel/anykernel3/anykernel.sh
+++ kali-nethunter-kernel/anykernel3/anykernel.sh
@@ -1,52 +1,42 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
-# Changes for SkyDragon by HolyAngel @ xda-developers
-# Changes for NetHunter by Re4son
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NetHunter Kernel for the OnePlus 7 Pro
+kernel.string=Nethunter Kernel for Realme 5/5i/5s/5NFC
 do.devicecheck=1
-do.modules=1
-do.systemless=0 #Never use this for NetHunter kernels as it prevents us from writing to /lib/modules
+do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=OnePlus7Pro
-device.name2=guacamole
-device.name3=OnePlus 7 Pro
-device.name4=Guacamole
-device.name5=OnePlus7
-device.name6=guacamoleb
-device.name7=OnePlus 7
-device.name8=Guacamoleb
-device.name9=OnePlus7ProNR
-device.name10=OnePlus7ProTMO
-device.name11=hotdogb
-device.name12=hotdog
-device.name13=OnePlus7T
-device.name14=OnePlus7TPro
+device.name1=RMX1911
+device.name2=RMX1925
+device.name3=RMX1927
+device.name4=RMX2030
+device.name5=realme_trinket
+device.name6=r5x
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=1;
+boot=/dev/block/by-name/boot;
+dtbo=/dev/block/by-name/dtbo;
+is_slot_device=0;
 ramdisk_compression=auto;
+
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-## Trim partitions
-fstrim -v /cache;
-fstrim -v /data;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-chmod -R 750 $ramdisk/*;
-chown -R root:root $ramdisk/*;
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
 
 ## AnyKernel install
 dump_boot;
